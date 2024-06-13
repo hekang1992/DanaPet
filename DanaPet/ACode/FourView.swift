94f58c0e8b73b487ddb3d7dc124effa3c355140d
何康 <hekang19921111@hotmail.com>
2024-06-13 16:23:59 +0800

    add:新增个人中心页面

--- DanaPet/ACode/FourView.swift
+++ DanaPet/ACode/FourView.swift
@@ -11,6 +11,16 @@ class FourView: UIView {
     
     var block: (() -> Void)?
     
+    var block1: (() -> Void)?
+    
+    var block2: (() -> Void)?
+    
+    var block3: (() -> Void)?
+    
+    var block4: (() -> Void)?
+    
+    var block5: (() -> Void)?
+    
     lazy var scrollView: UIScrollView = {
         let scrollView = UIScrollView()
         scrollView.showsVerticalScrollIndicator = false
@@ -51,6 +61,41 @@ class FourView: UIView {
         return nameLabel
     }()
     
+    lazy var btn1: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.setImage(UIImage(named: "Slicepmvmf"), for: .normal)
+        btn.addTarget(self, action: #selector(allClick), for: .touchUpInside)
+        return btn
+    }()
+    
+    lazy var btn2: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.setImage(UIImage(named: "Slicesupport"), for: .normal)
+        btn.addTarget(self, action: #selector(supportClick), for: .touchUpInside)
+        return btn
+    }()
+    
+    lazy var btn3: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.setImage(UIImage(named: "Sliceterms"), for: .normal)
+        btn.addTarget(self, action: #selector(termClick), for: .touchUpInside)
+        return btn
+    }()
+    
+    lazy var btn4: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.setImage(UIImage(named: "Slicedeldfjaftv"), for: .normal)
+        btn.addTarget(self, action: #selector(deleClick), for: .touchUpInside)
+        return btn
+    }()
+    
+    lazy var btn5: UIButton = {
+        let btn = UIButton(type: .custom)
+        btn.setImage(UIImage(named: "Slicedeldfjaftvrfdcx"), for: .normal)
+        btn.addTarget(self, action: #selector(logoutClick), for: .touchUpInside)
+        return btn
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         addSubview(scrollView)
@@ -59,6 +104,11 @@ class FourView: UIView {
         bgImageView.addSubview(btn)
         bgImageView.addSubview(nameLabel)
         scrollView.addSubview(bgImage1View)
+        scrollView.addSubview(btn1)
+        scrollView.addSubview(btn2)
+        scrollView.addSubview(btn3)
+        scrollView.addSubview(btn4)
+        scrollView.addSubview(btn5)
         scrollView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
@@ -86,6 +136,34 @@ class FourView: UIView {
             make.size.equalTo(CGSize(width: 231.pix(), height: 127.pix()))
             make.bottom.equalToSuperview().offset(-30.pix())
         }
+        btn1.snp.makeConstraints { make in
+            make.top.equalTo(bgImage2View.snp.bottom).offset(20.pix())
+            make.centerX.equalToSuperview()
+            make.left.equalToSuperview().offset(18.pix())
+            make.size.equalTo(CGSize(width: 335.pix(), height: 73.pix()))
+        }
+        btn2.snp.makeConstraints { make in
+            make.top.equalTo(btn1.snp.bottom).offset(20.pix())
+            make.centerX.equalToSuperview()
+            make.left.equalToSuperview().offset(18.pix())
+            make.size.equalTo(CGSize(width: 335.pix(), height: 73.pix()))
+        }
+        btn3.snp.makeConstraints { make in
+            make.top.equalTo(btn2.snp.bottom).offset(20.pix())
+            make.centerX.equalToSuperview()
+            make.left.equalToSuperview().offset(18.pix())
+            make.size.equalTo(CGSize(width: 335.pix(), height: 73.pix()))
+        }
+        btn4.snp.makeConstraints { make in
+            make.left.equalTo(btn1.snp.left)
+            make.top.equalTo(btn3.snp.bottom).offset(40.pix())
+            make.size.equalTo(CGSize(width: 160.pix(), height: 73.pix()))
+        }
+        btn5.snp.makeConstraints { make in
+            make.right.equalTo(btn1.snp.right)
+            make.top.equalTo(btn3.snp.bottom).offset(40.pix())
+            make.size.equalTo(CGSize(width: 160.pix(), height: 73.pix()))
+        }
     }
     
     required init?(coder: NSCoder) {
@@ -99,4 +177,26 @@ extension FourView {
     @objc func backClick() {
         self.block?()
     }
+    
+    @objc func allClick() {
+        self.block1?()
+    }
+    
+    @objc func supportClick() {
+        self.block2?()
+    }
+    
+    @objc func termClick() {
+        self.block3?()
+    }
+    
+    @objc func deleClick() {
+        self.block4?()
+    }
+    
+    @objc func logoutClick() {
+        self.block5?()
+    }
 }
+
+
